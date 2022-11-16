818c9c05908a751588c2c2fdd04eea2fdae9c0bb
alexsincai <alex.sincai@yahoo.co.uk>
2022-11-16 15:35:35 +0200

    state management and conditional rendering

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,19 @@
 import { Link } from "react-router-dom";
 
-const NavBar = () => <nav>
-    <Link to="/">Home</Link>
-    <Link to="/ceva">Ceva</Link>
-    <Link to="/altceva">Altceva</Link>
-</nav>
+import { useAtom } from "jotai";
+import state from "../state";
+
+const NavBar = () => {
+    const [logat] = useAtom(state.logat);
+
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/ceva">Ceva</Link>
+            <Link to="/altceva">Altceva</Link>
+            {logat && "Hello"}
+        </nav>
+    );
+};
 
 export default NavBar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,2 +1,21 @@
-const Home = () => "Home";
+import { useAtom } from "jotai";
+import state from "../state";
+
+const Home = () => {
+    const [logat, setLogat] = useAtom(state.logat);
+
+    const clickHandler = (e) => {
+        e.preventDefault();
+
+        setLogat(!logat);
+    };
+
+    return (
+        <div>
+            <button onClick={clickHandler}>Click me</button>
+
+            {logat ? <p>Esti logat</p> : <p>Nu esti logat</p>}
+        </div>
+    );
+};
 export default Home;
