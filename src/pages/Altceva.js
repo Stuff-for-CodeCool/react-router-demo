0fad500afff69669ac478bdfb65441635db1af24
alexsincai <alex.sincai@yahoo.co.uk>
2022-11-16 15:48:56 +0200

    loading external data

--- src/pages/Altceva.js
+++ src/pages/Altceva.js
@@ -1,2 +1,33 @@
-const Altceva = () => "Altceva";
+import { useEffect, useRef, useState } from "react";
+
+import { useAtom } from "jotai";
+import state from "../state";
+
+const Altceva = () => {
+    const altcevaRef = useRef(null);
+    const [posts, setPosts] = useState(null);
+    const [logat] = useAtom(state.logat);
+
+    const loader = async () => {
+        const req = await fetch("https://jsonplaceholder.typicode.com/todos/");
+        const res = await req.json();
+        setPosts(res);
+    };
+
+    // useEffect(() => {
+    //     setTimeout(loader, 1000);
+    // }, []);
+
+    useEffect(() => {
+        setTimeout(() => {
+            if (logat) loader();
+        }, 1000);
+    }, [logat]);
+
+    return (
+        <pre ref={altcevaRef}>
+            <code>{JSON.stringify(posts, null, 2)}</code>
+        </pre>
+    );
+};
 export default Altceva;
